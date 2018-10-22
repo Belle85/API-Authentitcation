c597f69617a6eac87ef434a95a821d553bc3c0df
Belle <bebold.bebelle@gmail.com>
2018-10-21 21:24:55 -0400

    Set up bcrypt to hash the password.

--- models/user.js
+++ models/user.js
@@ -1,19 +1,36 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 //Create a schema
 const userSchema = new Schema({
-    email:{
-        type: String,   
+    email: {
+        type: String,
         required: true,
         unique: true,
         lowercase: true
-    }, 
+    },
     password: {
-        type: String,   
+        type: String,
         required: true,
-    } 
+    }
 });
+
+userSchema.pre("save", async function (next) {
+    try {
+
+        //Generate a salt
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(this.password, salt);
+        console.log("salt", salt);
+        console.log("normal password", this.password);
+        console.log("hashed password", passwordHash);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
 
 //Create a model
 const User = mongoose.model("user", userSchema);
